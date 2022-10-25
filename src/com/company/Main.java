5d1a6808a74149936dfe92f643c4b5d3207a85ff
WayneJr <40968414+WayneJr@users.noreply.github.com>
2022-10-25 17:12:50 +0100

    conditionals commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,13 +20,15 @@ public class Main {
     // As a convention in java, we name classes starting with a capital letter and proceeding with camel case
     // e.g MainComponentClass
     // For variables and methods on the other hand, we start with a small letter and subsequent letters will
-    // use camel casing e.g mainComponentMethod
+    // use camel casing e.g mainComponentVariable
+    // for methods, they should start with verbs: getMainComponent()
 
-    // We say Java is a statically typed language, this is because all variables or methods declared in Java
-    // must have a type attached to it.
+    // We say Java is a statically typed language, this is because all variables or
+    // methods declared in Java must have a type attached to it.
 
-    // Concatenation is the process of joining two strings using the + operator, any primitive value combined with a
-    // string using the + operator is also concatenated to the string.
+    // Concatenation is the process of joining two strings using the + operator,
+    // N.B: Any primitive value combined with a string using the + operator is
+    // also concatenated to the string.
 
     /**
      * documentation comment
@@ -34,11 +36,16 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        // A bit also binary digit i.e 1 or 0
+        // 1 byte = 8 bits
+        // 4 bytes = 32 bits
+
         // primitive datatypes
         /*
         - boolean - true or false
-        - short - numbers - 2 bytes
-        - int - whole numbers e.g 1, 2, 3, 23, 55 - 4 bytes
+        - byte - -128 to 127 (2^8=256)
+        - short - numbers - 2 bytes  -32768 to 32767
+        - int - whole numbers e.g 1, 2, 3, 23, 55 - 4 bytes - -2147483648 to 2147483647
         - long - whole numbers e.g 1, 2, 3, 23, 55 - 8 bytes
         - float - decimal - 4 bytes
         - double - decimal - 8 bytes
@@ -49,6 +56,7 @@ public class Main {
         // A variable is a container or box for storing data in our code. You can think of it
         // like a box that's labelled.
 
+/*
         // declaration (declare)
         int number;
         boolean flag;
@@ -59,7 +67,7 @@ public class Main {
         flag = false;
         text = "Happy Feet";
 
-        float secondNum = 7;
+        int secondNum = 7;
 
         // Operators
         // Java supports the basic arithmetic operators
@@ -89,12 +97,33 @@ public class Main {
         // Modulus - returns the remainder of a division operation
         result = secondNum % number;
         System.out.println("Modulus: " + result);
+*/
 
 
+        // conditionals
+        // comparative logical operators
+        /*
+        - Greater than >
+        - Less than <
+        - Equals ==
+        - Greater than or equal >=
+        - Less than or equal <=
+        - Not equal !=
+        * */
+
+        int num1 = 5;
+        int num2 = 10;
+
+        boolean expression = num1 != num2;
+
+        if (num1 > num2) {
+            System.out.println("Num1 is greater!");
+        } else {
+            System.out.println("Num2 is greater!");
+        }
 
 
-
-//        System.out.println(text);
+//        System.out.println(expression);
 
     }
 }
